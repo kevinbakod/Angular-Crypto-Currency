15447e1eef540b230be082521f4bf11a6a6d3dbd
kevinbakod <captain_levi21@yahoo.com>
2018-03-02 08:49:10 +0800

    data.service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map'; 
 
 @Injectable()
 export class DataService {
 
-  constructor() { }
+	result:any;
+
+  constructor(private_http: Http) { }
+
+  getPrices(){
+  	return this.http.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,DASH&tsyms=BTC,USD,EUR')
+  		.map(result => this.result = result.json());
+  }
 
 }
